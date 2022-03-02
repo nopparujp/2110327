5ff0eaa39f1cde96c0e3065dd438e0547b5384b4
nopparuj917 <nopparuj.ta@gmail.com>
2022-03-02 18:52:25 +0700

    a

---
+++ ex02e3_bs/ex03e3_bs.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int search(vector<int> &v, int &query) {
+    int begin = 0;
+    int end = v.size()-1;
+    int mid;
+    if (query < v[0])
+        return -1;
+    if (query >= v[end])
+        return end;
+    while (end - begin  != 1) {
+    mid = (begin + end) >> 1;
+        // cout << end - begin << endl;
+        // cout << "end "  <<  end << endl;
+        // cout <<"begin " << begin << endl;
+        // cout <<"mid" << mid << endl;
+        if (v[mid] > query) {
+            end = mid;
+        } else {
+            begin = mid;
+        }
+    }
+        return begin;
+}
+int main() {
+    int n, m, query;
+    cin >> n >> m;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    for (int i = 0; i < m; ++i) {
+        cin >> query;
+        cout << search(v,query) << "\n";
+    }
+}
