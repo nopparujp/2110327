365f9ca38a01b2dc6ba336bf4b580a2f4e9d3fa2
nopparuj917 <nopparuj.ta@gmail.com>
2022-03-14 08:03:24 +0700

    a

--- a63_q1a_101/recursive.cpp
+++ a63_q1a_101/recursive.cpp
@@ -4,14 +4,14 @@ int con = (int)1e8 + 7;
 vector<int> cal(vector<int> &v, int t) {
     if (t == 2)
         return v;
-    // vector<int> tmp = cal(v, t - 1);
+    vector<int> tmp = cal(v, t - 1);
     vector<int> r(4);
-    // r[0] = (tmp[0] + tmp[2]) % con;
-    // r[1] = tmp[0];
-    // r[2] = (tmp[1] + tmp[3]) % con;
-    // r[3] = (tmp[3] + tmp[1]) % con;
+    r[0] = (tmp[0] + tmp[2]) % con;
+    r[1] = tmp[0];
+    r[2] = (tmp[1] + tmp[3]) % con;
+    r[3] = (tmp[3] + tmp[1]) % con;
     return r;
-    // return v;
+    
 }
 int main() {
     ios::sync_with_stdio(false);
--- a63_q1a_101/table1.cpp
+++ a63_q1a_101/table1.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int con = (int)1e8 + 7;
-vector<int> cal(vector<int> &v, int t) {
-    for(int i = 3; i <= t;++i){
+void cal(vector<int> &v, int t) {
+    for (int i = 3; i <= t; ++i) {
         vector<int> tmp = v;
-    v[0] = (tmp[0] + tmp[2]) % con;
-    v[1] = tmp[0];
-    v[2] = (tmp[1] + tmp[3]) % con;
-    v[3] = (tmp[3] + tmp[1]) % con;
-
+        v[0] = (tmp[0] + tmp[2]) % con;
+        v[1] = tmp[0];
+        v[2] = (tmp[1] + tmp[3]) % con;
+        v[3] = (tmp[3] + tmp[1]) % con;
     }
-    return v;
 }
 int main() {
     ios::sync_with_stdio(false);
@@ -18,8 +16,8 @@ int main() {
     int n, sum = 0;
     cin >> n;
     vector<int> v(4, 1);
-    vector<int> tmp = cal(v, n);
-    for (auto &i : tmp) {
+    cal(v,n);
+    for (auto &i : v) {
         sum += i;
         sum %= con;
     }
