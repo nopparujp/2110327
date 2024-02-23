3fa1d67ed0775fd463dbd0a6c03f1353017f0ecf
nopparuj917 <nopparuj.ta@gmail.com>
2024-02-23 18:27:03 +0700

    coin change bottom up

--- ex03e3_​coin/ex03e3_coin.cpp
+++ ex03e3_​coin/ex03e3_coin.cpp
@@ -2,16 +2,47 @@
 using namespace std;
 int main()
 {
-    int n, c, coin;
+    int n_coin, cash, coin;
     vector<int> vc;
-    cin >> n >> c;
-    for (int i = 0; i < n; ++i)
+    cin >> n_coin >> cash;
+    vector<int> dp(cash + 1, cash + 1);
+    dp[0] = -1;
+    for (int i = 0; i < n_coin; ++i)
     {
         cin >> coin;
         vc.push_back(coin);
     }
-    for (int i = 0; i < n; ++i)
+    for (int i = 1; i < cash + 1; ++i)
     {
-        cout << vc[i] << endl;
+        // check coin
+        bool is_one = false;
+        for (int j = 0; j < n_coin; ++j)
+        {
+            if (vc[j] == i)
+            {
+                dp[i] = 1;
+                is_one = true;
+            }
+        }
+
+        // find coin in vc
+        if (!is_one)
+        {
+            for (int k = 1; k < i; ++k)
+            {
+                for (int j = 0; j < n_coin; ++j)
+                {
+                    if (dp[k] != -1 && k + vc[j] == i && dp[k] + 1 < dp[i])
+                    {
+                        dp[i] = dp[k] + 1;
+                    }
+                }
+            }
+        }
     }
+    if (dp[cash] > cash)
+    {
+        cout << -1 << endl;
+    } else
+        cout << dp[cash] << endl;
 }
